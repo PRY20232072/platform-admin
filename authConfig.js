e6d010cb6eac2d68c907bfc980e08d5ace4867c0
MechaXAI <adrianxalzamora@gmail.com>
2024-04-18 00:04:47 -0500

    feat: updated root layout and home components with enhanced authentication and role-based views

---
+++ authConfig.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+// Config object to be passed to Msal on creation
+export const msalConfig = {
+  auth: {
+    clientId: process.env.AZURE_AD_B2C_CLIENT_ID,
+    authority: process.env.AZURE_AUTHORITY,
+    redirectUri: "/",
+    postLogoutRedirectUri: "/",
+    clientCapabilities: ["CP1"],
+  },
+  policies: {
+    authorities: {
+      signUpSignIn: {
+        authority: process.env.AZURE_AUTHORITY,
+      },
+    },
+  },
+  cache: {
+    cacheLocation: "localStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+  system: {
+    allowNativeBroker: false, // Disables WAM Broker
+  },
+};
+
+// Add here scopes for id token to be used at MS Identity Platform endpoints.
+export const loginRequest = {
+  scopes: ["User.Read", "openid", "profile", "User.ReadWrite.All"],
+};
+
+export const graphConfig = {
+  graphMeEndpoint: "https://graph.microsoft.com/beta/me",
+  graphUsersEndpoint: "https://graph.microsoft.com/beta/users",
+};
---
+++ components/admin/Welcome.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useGraphData } from "@/hooks/useGraphData";
+import msgraphService from "@/services/msgraphService";
+import { useState, useEffect } from "react";
+import { emptyAdmin } from "@/data/data";
+
+export default function Welcome() {
+  const [graphData, setGraphData] = useState({} as any);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await msgraphService.getMe();
+        setGraphData(data);
+      } catch (error) {
+        console.error("Failed to fetch graph data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className='flex flex-col  border-b border-gray-200 items-start gap-10 px-4 sm:px-6 md:px-8 lg:px-20 xl:px-24 2xl:px-32 py-10 relative flex-[0_0_auto]'>
+        <div className='w-full sm:w-3/4 z-10 font-bold text-4xl leading-12 relative mt-[-1.00px]'>
+          Hola {graphData?.displayName}
+        </div>
+      </div>
+    </>
+  );
+}
