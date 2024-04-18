958911441e20d66f94c02504ee0fb326cf99ab55
MechaXAI <adrianxalzamora@gmail.com>
2024-04-18 00:46:45 -0500

    feat: implemented users details information page

---
+++ app/admin/[adminId]/(routes)/patients/[patientId]/components/patient-basic-information.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { Card, CardBody } from "@nextui-org/react";
+import { emptyPatient } from "@/data/data";
+import CustomSuspense from "@/components/ui/custom-suspense";
+import FormSkeleton from "@/components/ui/skeletons/form-skeleton";
+import { Input } from "@nextui-org/react";
+
+export default function PatientBasicInformation({ userData }) {
+  return (
+    <CustomSuspense isLoading={userData === null} fallback={<FormSkeleton />}>
+      <Card>
+        <CardBody className='items-stretch self-stretch shadow  flex flex-col mt-2.5 p-5 rounded-2xl max-md:max-w-full'>
+          <div className='mb-8'>
+            <div className='mb-4 font-bold text-2xl tracking-[0] leading-[24px]'>
+              Información general
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+              <Input
+                isReadOnly
+                type='text'
+                label='Nombre completo'
+                labelPlacement='outside'
+                placeholder='No disponible'
+                value={userData.givenName}
+              />
+              <Input
+                isReadOnly
+                type='email'
+                label='Correo'
+                labelPlacement='outside'
+                placeholder='No disponible'
+                value={userData?.identities[0].issuerAssignedId}
+              />
+              <Input
+                isReadOnly
+                type='phone'
+                label='Teléfono'
+                labelPlacement='outside'
+                placeholder='No disponible'
+                value={
+                  userData.extension_25549976ad3f4b408bd442ed65100575_PhoneNumber
+                }
+              />
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    </CustomSuspense>
+  );
+}
---
+++ app/admin/[adminId]/(routes)/practitioners/[practitionerId]/components/practitioner-basic-information.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { Card, CardBody } from "@nextui-org/react";
+import CustomSuspense from "@/components/ui/custom-suspense";
+import FormSkeleton from "@/components/ui/skeletons/form-skeleton";
+import { Input } from "@nextui-org/react";
+
+export default function PractitionerBasicInformation({
+  userData,
+}: {
+  userData: any;
+}) {
+  return (
+    <CustomSuspense isLoading={userData === null} fallback={<FormSkeleton />}>
+      <Card>
+        <CardBody className='items-stretch self-stretch shadow  flex flex-col mt-2.5 p-5 rounded-2xl max-md:max-w-full'>
+          <div className='mb-8'>
+            <div className='mb-4 font-bold text-2xl tracking-[0] leading-[24px]'>
+              Información general
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+              <Input
+                isReadOnly
+                type='text'
+                label='Nombre completo'
+                labelPlacement='outside'
+                placeholder='No disponible'
+                value={userData.givenName}
+              />
+              <Input
+                isReadOnly
+                type='email'
+                label='Correo'
+                labelPlacement='outside'
+                placeholder='No disponible'
+                value={userData?.identities[0].issuerAssignedId}
+              />
+              <Input
+                isReadOnly
+                type='phone'
+                label='Teléfono'
+                labelPlacement='outside'
+                placeholder='No disponible'
+                value={
+                  userData.extension_25549976ad3f4b408bd442ed65100575_PhoneNumber
+                }
+              />
+            </div>
+          </div>
+          <div className='mb-8'>
+            <div className='mb-4 font-bold  text-2xl tracking-[0] leading-[24px]'>
+              Información de la organización
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+              <Input
+                isReadOnly
+                size='md'
+                type='email'
+                label='Correo institucional'
+                labelPlacement='outside'
+                placeholder='No disponible'
+                value={"No disponible"}
+              />
+              <Input
+                isReadOnly
+                type='Telefono'
+                label='Número de telefono'
+                labelPlacement='outside'
+                placeholder='No disponible'
+                value={"No disponible"}
+              />
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    </CustomSuspense>
+  );
+}
